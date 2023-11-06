9be47ae9a584b740a19661ef9ca435ccecfbda2f
Matheus Oliveira <matheusoliveirasouzaa@gmail.com>
2023-11-06 15:07:53 -0300

    Salvando as respostas fechadas dos alunos no BD

--- src/main/java/br/com/unincor/webSite/controller/BeanRespostaAluno.java
+++ src/main/java/br/com/unincor/webSite/controller/BeanRespostaAluno.java
@@ -3,6 +3,7 @@ package br.com.unincor.webSite.controller;
 import br.com.unincor.webSite.model.dao.AlunoDao;
 import br.com.unincor.webSite.model.dao.QuestaoDao;
 import br.com.unincor.webSite.model.dao.RespostaAlunoDao;
+import br.com.unincor.webSite.model.domain.Atividade;
 import br.com.unincor.webSite.model.domain.Questao;
 import br.com.unincor.webSite.model.domain.QuestaoFechada;
 import br.com.unincor.webSite.model.domain.RespostaAluno;
@@ -25,81 +26,72 @@ public class BeanRespostaAluno implements Serializable {
 
     private String codigo = "617ab";
     private List<Questao> questoes;
-
-    private Questao questaoAtual;
+    //private Questao questaoAtual;
     private Integer indexListQuestoes;
     private QuestaoFechada respQuestaoFechada;
-    private String respQuestaoAberta;
+    private String respQuestaoAberta = "";
     private List<RespostaAluno> respostaAlunosProvisoria;
+    //private String page;
 
     @PostConstruct
     public void init() {
         questoes = new ArrayList<>();
         questoes = new QuestaoDao().buscarQuestoesPorCodigoAtividade(codigo);
         indexListQuestoes = 0;
+        respostaAlunosProvisoria = new ArrayList<>();
     }
 
     public Questao getQuestaoAtual() {
         return questoes.get(indexListQuestoes);
     }
-//     
+   
+    
+    public void adicionaAlunoResposta(QuestaoFechada qf){
+        System.out.println("--------------");
+        System.out.println(qf.getDescricao());
+        System.out.println("--------------");
+        
+         FacesContext facesContext = FacesContext.getCurrentInstance();
+        HttpSession session = (HttpSession) facesContext.getExternalContext().getSession(false);
+        var alunoLogado = new AlunoDao().findById((Integer) session.getAttribute("alunoId"));
+        var atividade = qf.getQuestao().getAtividades();
 
-    public void adicionaAlunoResposta(Questao questao) {
+        if (qf.getQuestao().getTipoQuestao()) {
+            // Obtém a alternativa selecionada
+            QuestaoFechada alternativaSelecionada = respQuestaoFechada;
+
+            var respostaAluno = new RespostaAluno(null, null, qf, alunoLogado, null, qf.getQuestao());
+            System.out.println("resp aluno" + respostaAluno.getQuestaoFechada());
+            respostaAlunosProvisoria.add(respostaAluno);
+        }
+//        } else {
+//            var respostaAluno = new RespostaAluno(null, respQuestaoAberta, null, alunoLogado, atividade, questao);
+//            respostaAlunosProvisoria.add(respostaAluno);
+//        }
+    }
+    
+    public void adicionaAlunoRespostaAberta(Questao ques){
         FacesContext facesContext = FacesContext.getCurrentInstance();
         HttpSession session = (HttpSession) facesContext.getExternalContext().getSession(false);
         var alunoLogado = new AlunoDao().findById((Integer) session.getAttribute("alunoId"));
-        var atividade = questao.getAtividades().get(0);
-        if (questao.getTipoQuestao() == true) {
-            var respostaAluno = new RespostaAluno(null, null, respQuestaoFechada, alunoLogado, atividade, questao);
-            respostaAlunosProvisoria.add(respostaAluno);
-//             new RespostaAlunoDao().save(respostaAluno);
-        } else {
-            var respostaAluno = new RespostaAluno(null, respQuestaoAberta, null, alunoLogado, atividade, questao);
-            respostaAlunosProvisoria.add(respostaAluno);
-//             new RespostaAlunoDao().save(respostaAluno);
-
-        }
-
+        var atividade = ques.getAtividades();
+        
+        var respostaAluno = new RespostaAluno(null, respQuestaoAberta, null, alunoLogado, null, ques);
+        System.out.println("resposta questão aberta " + respQuestaoAberta);
+           respostaAlunosProvisoria.add(respostaAluno);
     }
-
+    
+ 
     public List<QuestaoFechada> getAlternativasQuestãoFechada(Questao questao) {
         return new RespostaAlunoDao().recuperaDescricaoQuestaoFechada(questao);
     }
-     
-    public void salvaRespostasAluno(){
+
+    public void salvaRespostasAluno() {
+        System.out.println("cai no salva, lista: " + respostaAlunosProvisoria);
         for (int i = 0; i < respostaAlunosProvisoria.size(); i++) {
+         
             new RespostaAlunoDao().save(respostaAlunosProvisoria.get(i));
         }
     }
 
-
-
-
-
-//     public void proxQuestao(){
-//         if(questoes.isEmpty()){
-//             System.out.println("A lista está vazia");
-//             return;
-//         }else{
-//             if(questaoAtual.getTipoQuestao() == true){
-//                 //colocar a lógica aqui
-//             }else{
-//                //colocar a outra lógica
-//             }
-//             indexListQuestoes++;
-//             if(questoes.get(indexListQuestoes)!= null){
-//                 this.questaoAtual=questoes.get(indexListQuestoes);
-//             }
-//         }
-//     }
-//     
-//     public void antQuestao(){
-//         if(indexListQuestoes <= 0){
-//             return;
-//         }else{
-//             indexListQuestoes--;
-//             this.questaoAtual=questoes.get(indexListQuestoes);
-//         }
-//     }
-//    
 }
--- src/main/java/br/com/unincor/webSite/view/utils/RespostaFechadaConverter.java
+++ src/main/java/br/com/unincor/webSite/view/utils/RespostaFechadaConverter.java
@@ -1,4 +1,3 @@
-
 package br.com.unincor.webSite.view.utils;
 
 import br.com.unincor.webSite.model.dao.QuestaoFechadaDao;
@@ -8,28 +7,85 @@ import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
 import javax.faces.convert.FacesConverter;
 
-
-
 @FacesConverter("respostaFechadaConverter")
-public class RespostaFechadaConverter implements Converter{
+//@FacesConverter(forClass = QuestaoFechada.class)
+public class RespostaFechadaConverter implements Converter {
 
     @Override
     public Object getAsObject(FacesContext fc, UIComponent uic, String value) {
-         if (value != null && !value.isBlank()) {
-            var questaoFechada = new QuestaoFechadaDao().findById(Long.valueOf(value));
-            return questaoFechada;
-
+        if (value != null && !value.isBlank()) {
+            return Long.valueOf(value);
         }
         return null;
     }
 
     @Override
     public String getAsString(FacesContext fc, UIComponent uic, Object value) {
-         if (value instanceof QuestaoFechada) {
-            QuestaoFechada alternativa = (QuestaoFechada) value;
-            return alternativa.getId().toString();
+        if (value instanceof Long) {
+            return String.valueOf((Long) value);
         }
-        return null;
+        return "";
     }
-       
+
+//    @Override
+//    public Object getAsObject(FacesContext fc, UIComponent uic, String value) {
+//        if (value != null && !value.isBlank()) {
+//           
+//            var test = new QuestaoFechadaDao().findById(Long.valueOf(value));
+//             System.out.println("1" + test);
+//            return test;
+//        }
+//        return null;
+//    }
+//
+//    @Override
+//    public String getAsString(FacesContext fc, UIComponent uic, Object value) {
+//        if (value instanceof QuestaoFechada) {
+//            QuestaoFechada questaoFechada = (QuestaoFechada) value;
+//            System.out.println("2" + String.valueOf(questaoFechada.getId()));
+//            return String.valueOf(questaoFechada.getId());
+//        }
+//        return "";
+//    }
+//    @Override
+//    public Object getAsObject(FacesContext context, UIComponent component, String value) {
+//        if (value != null && !value.isEmpty()) {
+//            return new QuestaoFechadaDao().findById(Long.valueOf(value));
+//        }
+//        return null;
+//    }
+//
+//    @Override
+//    public String getAsString(FacesContext context, UIComponent component, Object value) {
+//        if (value instanceof QuestaoFechada) {
+//            QuestaoFechada questaoFechada = (QuestaoFechada) value;
+//            return String.valueOf(questaoFechada.getId());
+//        }
+//        return "";
+//    }
+//    
+//    @Override
+//    public Object getAsObject(FacesContext fc, UIComponent uic, String value) {
+//        System.out.println("Estou no getAsObject");
+//        System.out.println(value);
+//        if (value != null && !value.isBlank()) {
+//            var qf = new QuestaoFechadaDao().findById(Long.valueOf(value));
+//              System.out.println("1" + qf.getId());
+//            return qf.getId();
+//         
+//        }
+//        return "";
+//    }
+//
+//    @Override
+//    public String getAsString(FacesContext fc, UIComponent uic, Object value) {
+//        System.out.println("Estou no getAsString");
+//        System.out.println(String.valueOf(value));
+//        if (value instanceof QuestaoFechada) {
+//            QuestaoFechada qf = (QuestaoFechada) value;
+//            System.out.println("2 "+ qf.getId().toString());
+//            return qf.getId().toString();
+//        }
+//        return "";
+//    }
 }
